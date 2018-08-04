080303e52be021a3c8e0f59cf83ad714b2b9f807
infernalslam <infernal-slam@hotmail.com>
2018-08-05 00:22:27 +0700

    fix code

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
-    <h1 class="is-size-1">{{ msg }}</h1>
+    <center><h1 class="is-size-1">{{ msg }}</h1></center>
     <form-data @onEnlargeText="onEnlargeText"></form-data>
+    <list-item :list="list"></list-item>
   </div>
 </template>
 
 <script>
 import FormData from './Form'
+import ListItem from './ListItem'
 export default {
   name: 'Index',
   components: {
-    FormData
+    FormData,
+    ListItem
   },
   data () {
     return {
       msg: 'Save your money in procesing!😋',
-      list: [],
-      text: '',
-      money: 0
+      list: []
+      // text: '',
+      // money: 0
     }
   },
   methods: {
     onEnlargeText (form) {
-      this.text = form.text
-      this.money = form.money
-      console.log(this.text, this.money)
+      if (form.text && (parseFloat(form.money) > 0)) {
+        this.list.push(form)
+      }
     }
   }
 }
---
+++ src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-half is-offset-one-quarter">
+        {{ list }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['list']
+}
+</script>
